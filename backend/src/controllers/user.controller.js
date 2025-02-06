ebf4b48a03aa313f859fbdf79f095ed3af76078b
Hemil-Hansora <hemilhansora1729@gmail.com>
2025-02-06 10:24:24 +0530

    implement user login, logout, and refresh token functionality with error handling

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -9,14 +9,12 @@ import mongoose from "mongoose";
 const generateAccessAndRefereshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
-    console.log(process.env.ACCESS_TOKEN_SECRTE)
-    console.log(process.env.REFRESH_TOKEN_SECRTE)
+    console.log(process.env.ACCESS_TOKEN_SECRTE);
+    console.log(process.env.REFRESH_TOKEN_SECRTE);
 
-    
-    
     const accessToken = user.generateAccessToken();
     const refereshToken = user.generateRefreshToken();
-    
+
     user.refreshToken = refereshToken;
     await user.save({ validateBeforeSave: false });
 
@@ -24,7 +22,8 @@ const generateAccessAndRefereshToken = async (userId) => {
   } catch (error) {
     throw new ApiError(
       500,
-      error.message || "Something went wrong while generating Access and Referesh Token "
+      error.message ||
+        "Something went wrong while generating Access and Referesh Token "
     );
   }
 };
@@ -35,7 +34,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const profilePicture = req.file;
- 
 
   const allowedFormate = ["image/png", "image/jpeg", "image/webp"];
 
@@ -141,60 +139,138 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-const loginUser = asyncHandler(async (req,res)=>{
-    const {email,password} = req.body
+  if (!email || !password) {
+    throw new ApiError(400, "Please fill the details");
+  }
 
-    if(!email || !password){
-        throw new ApiError(400,"Please fill the details")
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, " Invalid Credentials");
+  }
+
+  const isPasswordVaild = user.isPasswordCorrect(password);
+
+  if (!isPasswordVaild) {
+    throw new ApiError(400, "Invalid Credentials");
+  }
+
+  const { refereshToken, accessToken } = await generateAccessAndRefereshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refereshToken"
+  );
+
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, option)
+    .cookie("refreshToken", refereshToken, option)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refereshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const getprofile = asyncHandler(async (req, res) => {
+  const user = req.user;
+  res.status(200).json(new ApiResponse(200, user, "User profile fetched"));
+});
+
+const logout = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  await User.findByIdAndUpdate(
+    user._id,
+    {
+      $set: {
+        refreshToken: "",
+      },
+    },
+    { new: true }
+  );
+
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken",option)
+    .clearCookie("refreshToken",option)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+const refreshAccessToken  = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+  if(!incomingRefreshToken){
+    throw new ApiError(400, "Unauthorized access");
+  }
+
+  jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRTE, async (err, decodedToken) => {
+    if(err){
+      throw new ApiError(400, "Unauthorized access");
     }
 
-    const user = await User.findOne({email})
+    const user = await User.findById(decodedToken?._id);
 
     if(!user){
-        throw new ApiError(400 , " Invalid Credentials")
+      throw new ApiError(400, "Invalid refresh token");
     }
 
-    const isPasswordVaild = user.isPasswordCorrect(password)
-
-    if(!isPasswordVaild){
-        throw new ApiError(400, "Invalid Credentials")
+    if(incomingRefreshToken !== user.refreshToken){
+      throw new ApiError(400, "referesh token is expired or used");
     }
-
-    const { refereshToken, accessToken } = await generateAccessAndRefereshToken(
-        user._id
-    );
-
-    const loggedInUser = await User.findById(user._id).select(
-        "-password -refereshToken"
-    );
 
     const option = {
-        httpOnly: true,
-        secure: true,
+      httpOnly: true,
+      secure: true,
     };
 
-    return res
-        .status(200)
-        .cookie("accessToken", accessToken, option)
-        .cookie("refreshToken", refereshToken, option)
-        .json(
-            new ApiResponse(
-                200,
-                {
-                    user: loggedInUser,
-                    accessToken,
-                    refereshToken,
-                },
-                "User logged in successfully"
-            )
-        );
-})
+    const { refereshToken, accessToken } = await generateAccessAndRefereshToken(user._id);
 
-const getprofile = asyncHandler(async (req,res)=>{})
+    return res.status(200).cookie("accessToken", accessToken, option).cookie("refreshToken", refereshToken, option).json(new ApiResponse(200, {accessToken, refereshToken}, "Access token refreshed successfully"));  
+  }); 
+});
 
-const logout = asyncHandler(async (req,res)=>{})
+const fetchLeaderboard = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        moneySpent : { $gt: 0 }
+      }
+    },
+    {
+      $sort : {
+        moneySpent : -1
+      }
+    },
+    {
+      $limit : 10
+    }
+  ])
 
-const fetchLeaderboard = asyncHandler(async (req,res)=>{})
+  res.status(200).json(new ApiResponse(200, user, "Leaderboard fetched successfully"));
+});
 
-export { registerUser,getprofile,loginUser,logout,fetchLeaderboard };
+
+
+export { registerUser, getprofile, loginUser, logout, fetchLeaderboard };
